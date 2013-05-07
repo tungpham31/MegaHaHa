b4fa16fdc8c9c6845effc35be9e4119f1e6c93a8
Tung Pham <tungpham31@gmail.com>
2013-05-07 02:35:38 -0400

    Add Version Code To Parse Installation

--- src/com/awwstream/android/MainApplication.java
+++ src/com/awwstream/android/MainApplication.java
@@ -2,11 +2,11 @@ package com.awwstream.android;
 
 import android.app.Application;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 import com.parse.PushService;
-import com.revmob.RevMob;
 
 /**
  * The main {@link Application}.
@@ -27,16 +27,20 @@ public final class MainApplication extends Application {
         }
 
         // Set up push notification.
+        PushService.setDefaultPushCallback(this, HotActivity.class);
         PushService.subscribe(this, "Hot", HotActivity.class);
         PushService.subscribe(this, "New", NewActivity.class);
         try {
             final int versionCode =
                     getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
             PushService.subscribe(this, "Update-" + versionCode, UpdateActivity.class);
+            
+            // Update Parse Installation.
+            ParseInstallation parseInstallation = ParseInstallation.getCurrentInstallation();
+            parseInstallation.put("versionCode", Integer.valueOf(versionCode));
+            parseInstallation.saveInBackground();
         } catch (NameNotFoundException e) {
             // Do nothing.
         }
-        PushService.setDefaultPushCallback(this, HotActivity.class);
-        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
